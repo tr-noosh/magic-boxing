6cfe0493e40b282c5a131fd25c5ee2c59144e450
tr-noosh <kianooshfathi@cmail.carleton.ca>
2026-02-26 23:50:13 -0500

    dodge hit zones wip

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	public bool center = true;
+	public bool left = false;
+	public bool right = false;
+	public bool low = true;
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		
+	}
+}
